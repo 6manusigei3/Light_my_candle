b59db498535c0e9a970352d0f571614153eca310
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2025-12-22 19:08:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', functi () {
+document.addEventListener('DOMContentLoaded', functio () {
   const searchBar = document.getElementById('searchBar');
   if (!searchBar) return;
   searchBar.addEventListener('input', function () {
